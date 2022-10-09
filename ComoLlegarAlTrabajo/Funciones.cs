e89373dffbd936985cd1781019a874938c9d603a
Jhojan Toro-Study <jhojan1806.study@gmail.com>
2022-10-08 19:03:09 -0500

    Agregar archivos de proyecto.

---
+++ ComoLlegarAlTrabajo/Funciones.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComoLlegarAlTrabajo
+{
+    internal class Funciones
+    {
+        public static void aPie()
+        {
+
+        }
+        public static void auto()
+        {
+
+        }
+
+        public static void moto()
+        {
+
+        }
+        
+        public static void bus()
+        {
+
+        }
+
+        public static void taxi()
+        {
+
+        }
+
+
+    }
+}
---
+++ ComoLlegarAlTrabajo/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComoLlegarAlTrabajo
+{
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string work; 
+            Console.WriteLine("¿Vas al trabajo?");
+
+            work = Console.ReadLine();
+
+            if(work == "si")
+            {
+                Console.WriteLine("¿Vas en transporte?");
+                string transporte = Console.ReadLine();
+
+                if (transporte == "si")
+                {
+                    Console.WriteLine("¿Tienes Dinero?");
+                    string dinero = Console.ReadLine();
+                    while(dinero != "si")
+                    {
+                        Console.WriteLine("Consigue Dinero");
+                        Console.WriteLine("");
+
+                        Console.WriteLine("¿Ya tienes dinero?");
+                        dinero = Console.ReadLine();
+                    }
+                    Console.WriteLine("¿Que tipo de transporte busca?\n" +
+                        "1. auto\n" +
+                        "2. moto\n" +
+                        "3. bus\n" +
+                        "4. Taxi\n");
+
+                    int tTransporte = int.Parse(Console.ReadLine());
+
+                    switch (tTransporte)
+                    {
+                        case 1:
+                            Funciones.auto();
+                            break;
+                        case 2:
+                            Funciones.moto();
+                            break;
+                        case 3:
+                            Funciones.bus();
+                            break;
+                        case 4:
+                            Funciones.taxi(); 
+                            break;
+                        default:
+                            Console.WriteLine("Opcion invalida");
+                            break;
+                    }
+
+                    Console.WriteLine("¿Cuando sales del medio de transporte \n" +
+                        "tienes que caminar?");
+                    string caminar = Console.ReadLine();
+
+                    if(caminar == "si")
+                    {
+                        Funciones.aPie(); 
+                    }
+                    Console.WriteLine("Hoy no hay trabajo");
+                }
+                else
+                {
+                
+                    Console.WriteLine("¿tienes que caminar?");
+
+                    string caminar = Console.ReadLine();
+
+                    if (caminar == "si")
+                    {
+                    
+                        Funciones.aPie();
+                        
+                    }
+
+                }
+                
+                Console.WriteLine("Has llegado al trabajo"); 
+
+            }
+            else
+            {
+                Console.WriteLine("Hoy no hay trabajo");
+            }
+
+        }
+    }
+}
